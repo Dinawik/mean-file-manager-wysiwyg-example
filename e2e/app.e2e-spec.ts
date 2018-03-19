a661632b5ddaa6c9afd62283b8092f3e50bcd903
Yusuf Uğur ÖZBEK <yusufugurozbek@hotmail.com>
2018-03-19 13:24:05 +0100

    Project name is changed. Dev and Prod scripts are added. Below packages are installed.
    express, mongoose, morgan, multer

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('mean-file-upload-wysiwyg-example App', () => {
+describe('mean-file-manager-wysiwyg-example App', () => {
   let page: AppPage;
 
   beforeEach(() => {
